4e55fb5e435a76108fe0b10dc12c6b92c025b961
EscaG <escagt82@gmail.com>
2022-05-10 16:18:27 +0300

    fonts

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
-import './reset.scss';
-import './App.scss';
-import './index.css';
 import App from './App';
+import './index.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
